bc24dfcb6d23a6d81e76e84f973a245327b07c72
Olivier Chevallier <olivierchevallier@macbook-pro-dolivier.local>
2020-03-24 13:27:56 +0100

    Ajout de polices personnalisées.

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo'; 
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
@@ -8,9 +10,27 @@ import GameOverScreen from './screens/GameOverScreen';
 
 import Colors from './constants/colors';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+}
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading 
+        startAsync={ fetchFonts }
+        onFinish={ () => setDataLoaded(true) }
+        onError={ (error) => console.log(error) }   
+      />
+      );
+  }
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 12,
+    fontFamily: 'open-sans',
   },
 
   card: {
